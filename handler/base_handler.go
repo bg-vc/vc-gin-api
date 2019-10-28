5f82b3afd7109ec1e981a050813c1b1739c7d1fe
vincecfl <vincecfl@gmail.com>
2019-10-28 22:42:26 +0800

    update

--- handler/base_handler.go
+++ handler/base_handler.go
@@ -9,7 +9,7 @@ import (
 type Response struct {
 	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
-	Data interface{} `json:"data"`
+	Data interface{} `json:"data,omitempty"`
 }
 
 func SendResponse(c *gin.Context, err error, data interface{}) {
